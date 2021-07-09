32f3a4010d41fd2ac54fbd6c0e1842ca87a513fd
psmithCN <87032865+psmithCN@users.noreply.github.com>
2021-07-09 12:16:28 -0700

    first iteration

--- main.ts
+++ main.ts
@@ -1,46 +1,9 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    info.changeScoreBy(1)
-    music.baDing.play()
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeScoreBy(-1)
+    dog.say("Oh no!!")
 })
-let iceCream1 = sprites.create(img`
-    . . . . . 3 3 b 3 3 d d 3 3 . . 
-    . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
-    . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
-    . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
-    . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
-    . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
-    . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
-    . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
-    . 4 4 d 1 1 1 1 1 1 d d d b b . 
-    . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
-    4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
-    4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
-    4 5 5 d 5 5 d b b b d d 3 . . . 
-    4 5 5 5 d d d d 4 4 b 3 . . . . 
-    . 4 5 5 5 4 4 4 . . . . . . . . 
-    . . 4 4 4 . . . . . . . . . . . 
-    `, SpriteKind.Food)
-iceCream1.setPosition(23, 99)
-let iceCream2 = sprites.create(img`
-    . . . . . 3 3 b 3 3 d d 3 3 . . 
-    . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
-    . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
-    . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
-    . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
-    . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
-    . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
-    . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
-    . 4 4 d 1 1 1 1 1 1 d d d b b . 
-    . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
-    4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
-    4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
-    4 5 5 d 5 5 d b b b d d 3 . . . 
-    4 5 5 5 d d d d 4 4 b 3 . . . . 
-    . 4 5 5 5 4 4 4 . . . . . . . . 
-    . . 4 4 4 . . . . . . . . . . . 
-    `, SpriteKind.Food)
-iceCream2.setPosition(133, 102)
-let dog = sprites.create(img`
+let dog: Sprite = null
+dog = sprites.create(img`
     . . 4 4 4 . . . . 4 4 4 . . . . 
     . 4 5 5 5 e . . e 5 5 5 4 . . . 
     4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
@@ -59,4 +22,33 @@ let dog = sprites.create(img`
 dog.setPosition(70, 26)
 controller.moveSprite(dog)
 dog.setStayInScreen(true)
-info.setScore(0)
+let ghost = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .....fffc1111111f.......
+    ...fc111cd1111111f......
+    ...f1b1b1b1111dddf......
+    ...fbfbffcf11fcddf......
+    ......fcf111111bbf......
+    .......ccbdb1b1fcf......
+    .......fffbfbfdff.......
+    ........ffffffff........
+    ........fffffffffff.....
+    .........fffffc111cf....
+    .........fffff1b1b1f....
+    ..........ffffbfbfbf....
+    ...........ffff.........
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+ghost.setPosition(129, 98)
+ghost.follow(dog, 50)
+info.startCountdown(10)
+info.setScore(5)
