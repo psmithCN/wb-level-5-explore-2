2072a3712bdb0aa3eea9a8ccf50a1d6ef465133c
psmithCN <87032865+psmithCN@users.noreply.github.com>
2021-07-09 12:27:53 -0700

    added set image to starter code

--- main.ts
+++ main.ts
@@ -1,4 +1,22 @@
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    otherSprite.setImage(img`
+        . . . . . 3 3 . . . . . . . . . 
+        . . . . 3 1 1 3 . . . . . . . . 
+        . . . 3 d 1 1 3 . . . . . . . . 
+        . . 3 d d 1 1 3 . . . . . . . . 
+        . 3 1 1 d 1 1 3 3 . . . . . . . 
+        . 3 1 1 1 d 1 1 3 3 . . . . . . 
+        . b d 1 1 1 d 1 1 3 3 3 . . . . 
+        . 4 b 1 1 1 1 d d 1 1 3 3 . . . 
+        . 4 4 d 1 1 1 1 1 1 d d d b b . 
+        . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
+        4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
+        4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
+        4 5 5 d 5 5 d b b b d d 3 . . . 
+        4 5 5 5 d d d d 4 4 b 3 . . . . 
+        . 4 5 5 5 4 4 4 . . . . . . . . 
+        . . 4 4 4 . . . . . . . . . . . 
+        `)
     info.changeScoreBy(1)
     music.baDing.play()
 })
